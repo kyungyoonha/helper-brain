8c09bd7439934a4c89339a75baefc89c0e1e09d7
yoon <yoon@yoonui-MacBookPro.local>
2023-10-19 11:19:41 +0900

    -

--- src/configs/index.tsx
+++ src/configs/index.tsx
@@ -2,5 +2,5 @@ const env = process.env.NEXT_PUBLIC_BUILD_ENV || "";
 
 export const BACK_URL =
   env.replace(/\\n/g, "\n") === "prod"
-    ? "https://helper-brain.vercel.app/api"
+    ? "https://helper-brain.vercel.app"
     : "http://localhost:3000";
