dca7cb486c41e314cc75e889617b534fbd1a8a3a
yoon <yoon@yoonui-MacBookPro.local>
2023-10-19 10:51:17 +0900

    feat: change env prefix

--- src/configs/index.tsx
+++ src/configs/index.tsx
@@ -1,4 +1,4 @@
 export const BACK_URL =
-  process.env.REACT_APP_BUILD_ENV === "prod"
+  process.env.NEXT_PUBLIC_BUILD_ENV === "prod"
     ? "https://helper-brain.vercel.app/api"
     : "http://localhost:3000/api";
