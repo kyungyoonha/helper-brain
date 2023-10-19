4a090c95e4335d2c1a8951d80e56a35178f2845d
yoon <yoon@yoonui-MacBookPro.local>
2023-10-19 11:17:05 +0900

    Feat: change url

--- src/configs/index.tsx
+++ src/configs/index.tsx
@@ -1,4 +1,6 @@
+const env = process.env.NEXT_PUBLIC_BUILD_ENV || "";
+
 export const BACK_URL =
-  process.env.NEXT_PUBLIC_BUILD_ENV === "prod"
-    ? "https://helper-brain.vercel.app"
+  env.replace(/\\n/g, "\n") === "prod"
+    ? "https://helper-brain.vercel.app/api"
     : "http://localhost:3000";
